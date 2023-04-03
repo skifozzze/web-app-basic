f24dd5a5391a79a131eba3dd32bde32493fe1d47
skifozzze <skifozzze@gmail.com>
2023-04-03 15:12:49 +0300

    fin

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-const express = require('express');
-const exhbs = require('express-handlebars');
+import express from 'express';
+import { engine, create } from 'express-handlebars';
+import products from './products.json' assert { type: 'json' };
 
 const app = express();
 
 app.use(express.static('public'));
-app.set('view engine', 'hbs');
-app.engine(
-  'handlebars',
-  exhbs({
-    extname: 'hbs',
-    layoutsDir: __dirname + '/views/layouts',
-  })
-);
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 app.get('/', (req, res) => {
-  console.log('this is callback for app.get(" / ")');
-  console.log(req.url);
-  res.send('<h2> hello, this is /</h2>');
+  res.render('home', { pageTitle: 'Home' });
 });
 
 app.get('/about', (req, res) => {
-  console.log('this is callback for app.get(" /about ")');
-  console.log(req.url);
-  res.send('<h1> hello, this is /about </h1>');
+  res.render('about', { cssFileName: 'about', pageTitle: 'About' });
+});
+
+app.get('/products', (req, res) => {
+  res.render('products', {
+    products,
+    cssFileName: 'products',
+    pageTitle: 'Products',
+  });
+});
+
+app.get('/product/:productId', (req, res) => {
+  const product = products.find(p => p.id === req.params.productId);
+
+  res.render('product', { product, pageTitle: 'Product' });
 });
 
 app.listen(4444, () => {
